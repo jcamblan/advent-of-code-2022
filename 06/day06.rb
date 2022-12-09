8b17d2523213efa400d8073132ff546e34add576
Julien <julien.camblan@gmail.com>
2022-12-09 16:19:31 +0100

    refactor: use blocks

--- 06/day06.rb
+++ 06/day06.rb
@@ -5,22 +5,27 @@ require_relative '../puzzle'
 # Day 3: Rucksack Reorganization
 class Day06 < Puzzle
   def part1
-    input.chars.each.with_index do |letter, i|
-      next unless i >= 4 && !input.chars[i - 4..i - 1].include?(letter) && input.chars[i - 4..i - 1].uniq.count == 4
-
-      break i
+    find_marker do |i, input, letter|
+      i >= 4 && !input.chars[i - 4..i - 1].include?(letter) &&
+        input.chars[i - 4..i - 1].uniq.count == 4
     end
   end
 
   def part2
-    input.chars.each.with_index do |_letter, i|
-      next unless i >= 14 && input.chars[i - 14..i - 1].uniq.count == 14
-
-      break i
+    find_marker do |i, input, _|
+      i >= 14 && input.chars[i - 14..i - 1].uniq.count == 14
     end
   end
 
   private
+
+  def find_marker
+    input.chars.each.with_index do |letter, i|
+      next unless yield(i, input, letter)
+
+      break i
+    end
+  end
 
   def input
     @input ||= File.open('./06/input.txt').read
